b1ded05650d103a866b207b522586acdbbbf85f0
ababil1247 <kaziababilazamtalha@gmail.com>
2022-06-09 01:43:28 +0600

    correction to product class made

--- Offline 2/src/QueueManagementSystem/QMSProduct.java
+++ Offline 2/src/QueueManagementSystem/QMSProduct.java
@@ -3,44 +3,39 @@ package QueueManagementSystem;
 import java.util.ArrayList;
 
 class QMSProduct {
-    private ArrayList<Display> displays;
-    private Processor processor;
-    private CommModule commModule;
-    private Application app;
+    private ArrayList<Part> parts;
 
 
     public QMSProduct() {
-        this.displays = new ArrayList<>();
-        processor= null;
-        commModule= null;
-        app= new Application();
+        this.parts = new ArrayList<>();
+        this.parts.add(new Application());
     }
 
     public void addDisplays(Display d, int displayCount){
         for (int i = 0; i < displayCount; i++) {
-            this.displays.add(d);
+            this.parts.add(d);
         }
     }
 
     public void addProcessor(Processor p){
-        this.processor=p;
+        this.parts.add(p);
     }
 
     public void addCommModule(CommModule c){
-        this.commModule=c;
+        this.parts.add(c);
     }
 
     public void show(int year){
-        double totalSystemCost= processor.getPrice() + commModule.getPrice() + app.getPrice() + year*commModule.getYearlyCost();//
-        System.out.println("Display Unit:\nProcessor:\n"+processor+
-                "\nDisplays:");
-        System.out.println(displays.get(0)+" x "+displays.size());
-        for (int i = 0; i < displays.size(); i++) {
-            totalSystemCost+=displays.get(i).getPrice();
+        double totalCommunicationCost=0;
+        double totalSystemCost=0;
+        for (int i = 0; i < parts.size(); i++) {
+            System.out.println(parts.get(i));
+            totalSystemCost+=parts.get(i).getPrice();
+            if(parts.get(i) instanceof CommModule){
+                totalCommunicationCost+=year*((CommModule) parts.get(i)).getYearlyCost();
+            }
         }
-
-        System.out.println("Communication channel:\n"+commModule);
-        System.out.println("Application:\n"+app);
-        System.out.println("Cost of the total system (with "+year+" year(s) worth of communication cost): Tk. "+totalSystemCost+'\n');
+        System.out.println("Cost of the total queue management system: Tk. "+(totalSystemCost));
+        System.out.println("Cost of the total system (with "+year+" year(s) worth of communication cost): Tk. "+(totalSystemCost+totalCommunicationCost));
     }
 }
